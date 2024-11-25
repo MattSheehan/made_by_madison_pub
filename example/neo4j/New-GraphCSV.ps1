4d88916209f3d3471ebd5a72fc8293d9bd8523ab
Matt Sheehan <MattSheehan@users.noreply.github.com>
2024-11-24 22:44:10 -0700

    COMPLETE: Created automatic Graph-CSV script that builds an entire test DB (fuck yeah) ++created neo4jDB instances (desktop & sandbox ENVs) | TODO: Finish New-GraphCSV.ps1. FINALLY, finish udemy course with your own custom graph database!! == Create frontend that pulls data from neo4j (driver.js) using graphql & neo4j-react hooks (./api/*.gql ++ ./api/*.jsx)

--- example/neo4j/New-GraphCSV.ps1
+++ example/neo4j/New-GraphCSV.ps1
@@ -1,6 +1,5 @@
 <#  Copyright 2024 @Made_by_Madison | all rights reserved | ControlGroup: @MattSheehan  #>
 [Boolean]$authERROR, $configERROR, $seedERROR = ( $false ), ( $false ), ( $false );
-<#  AUTH  |  Set location, Elevate privileges of this script, and Authorizes the runtime  #>
 try { Set-Location $env:HOME/Desktop/apps/made_by_madison_pub ; Unblock-File ./example/neo4j/Create-GraphCSV.ps1 } catch {
   @('CurrentUser', 'Process') | ForEach-Object { try { Set-ExecutionPolicy 'Bypass' -Scope ( $_ ) -Force } catch { return } };
   try { Set-Location $env:HOME/Desktop/apps/made_by_madison_pub } catch { $authERROR = $true ; throw };
@@ -9,50 +8,51 @@ try { Set-Location $env:HOME/Desktop/apps/made_by_madison_pub ; Unblock-File ./e
 <#  CONFIG  |  GETTER(s) && SETTER(s)  #>
 if ( $authERROR ) { $configERROR = ( $true ); return } else {
   [String]$Neo4jPath = ("$env:HOME/Desktop/apps/made_by_madison_pub/example/neo4j");
-  [System.Collections.ArrayList]$SchemaNodes = @();
-  [Hashtable]$CSVSchema = @{
+  [System.Collections.ArrayList]$Set_CSVSchema = @();
+  [Int32]$node_i, $rel_i = ( 1 ), ( 9 );
+  [Hashtable]$Get_CSVSchema = @{
     _about   = [System.Collections.ArrayList]@("id", "aboutContent", "blogs", "docs", "contact", "tbd_0", "tbd_1", "tbd_2", "tbd_3", "tbd_4");
-    _auth    = [System.Collections.ArrayList]@("id", "userId", "username", "password", "email", "mobile", "loginDate", "loginState", "rbacID", "acctState");
-    _cart    = [System.Collections.ArrayList]@("id", "storeId", "productId", "buyerId", "sellerId", "price", "quantity", "dateAdded", "tbd_0");
-    _home    = [System.Collections.ArrayList]@("id", "featuredContent", "promotedContent", "sales", "seasonal", "recommendations", "profileContent", "isAuth", "tbd_0");
-    _product = [System.Collections.ArrayList]@("id", "shopId", "sellerId", "type", "name", "title", "subtitle", "imageUrl", "price", "currency", "color", "care", "quantity", "sizes");
-    _profile = [System.Collections.ArrayList]@("id", "username", "password", "email", "homeNumber", "dateCreated", "rbacID", "companyName", "companyNumber", "acctState");
-    _rbac    = [System.Collections.ArrayList]@("id", "roleID", "roleName", "accessID", "accessName", "description");
-    _shop    = [System.Collections.ArrayList]@("id", "name", "title", "subtitle", "imageUrl", "category", "productIDs");
-  } ; [Int32]$node_i, $rel_i = ( 1 ), ( 9 );
+    _auth    = [System.Collections.ArrayList]@("id", "userId", "username", "password", "email", "mobile", "loginDate", "rbacID", "acctState", "method1Pass", "method2Pass", "token");
+    _cart    = [System.Collections.ArrayList]@("id", "shopId", "productIDs", "instanceId", "buyerId", "sellerId", "price", "dateAdded", "isActive", "paymentMethod");
+    _home    = [System.Collections.ArrayList]@("id", "featuredContent", "promotionals", "sales", "seasonal", "recommendations", "profileContent", "isAuth");
+    _product = [System.Collections.ArrayList]@(
+      "id", "guid", "shopId", "sellerId", "type", "name", "title", "subtitle", "imageUrl", "price", "currency", "color", "care", "quantity", "sizes", "specials", "dateCreated"
+    );
+    _profile = [System.Collections.ArrayList]@("id", "username", "password", "email", "homeNumber", "dateSignedIn", "dateCreated", "rbacId", "companyName", "companyNumber", "acctState");
+    _rbac    = [System.Collections.ArrayList]@("id", "userId", "roleName", "accessId", "accessName", "homeNumber", "description", "acctState");
+    _shop    = [System.Collections.ArrayList]@("id", "name", "title", "subtitle", "imageUrl", "type", "category", "productIDs", "specials");
+  };
   @('About', 'Auth', 'Cart', 'Home', 'Product', 'Profile', 'RBAC', 'Shop') | ForEach-Object {
-    [Hashtable]$csvNode = @{
+    [Hashtable]$csvNode, $csvRelationship = @{
       _caption = [String]("na");
       _cols    = [System.Collections.ArrayList]@();
-      _guid    = [String]([System.Guid]::NewGuid().ToString());
+      _guid    = [String]("$([System.Guid]::NewGuid())");
       _id      = [String]("n$($node_i)");
       _label   = [String]("$_");
       _name    = [String]("na");
-      _rel     = [Hashtable]@{ id = [String]("n$( $rel_i )"); fromId = [String](""); toId = [String](""); type = [String](""); direction = [String](""); properties = [Hashtable]@{} };
       _rows    = [System.Collections.ArrayList]@()
+    }, @{id = [String]("n$( $rel_i )"); fromId = [String](""); toId = [String](""); type = [String](""); direction = [String](""); properties = [Hashtable]@{
+        id = [String]("$([System.Guid]::NewGuid())"); date = [String](""); userId = [String](""); storeId = [String](""); productId = [String]("")
+      }
     };
     switch -Wildcard ("$( $csvNode._label )") {
-      "*About*" { $csvNode._label = "$_"; $csvNode._cols += ($CSVSchema._about); $csvNode._name = ("about"); $csvNode._rows += @(); break }
-      "*Auth*" { $csvNode._label = "$_"; $csvNode._cols += ($CSVSchema._auth); $csvNode._name = ("auth"); $csvNode._rows += @(); break }
-      "*Cart*" { $csvNode._label = "$_"; $csvNode._cols += ($CSVSchema._cart); $csvNode._name = ("cart"); $csvNode._rows += @(); break }
-      "*Home*" { $csvNode._label = "$_"; $csvNode._cols += ($CSVSchema._home); $csvNode._name = ("home"); $csvNode._rows += @(); break }
+      "*About*" { $csvNode._label = "$_"; $csvNode._cols += ($Get_CSVSchema._about); $csvNode._name = ("about"); $csvNode._rows += @(",,,,,,,,,`n", ",,,,,,,,,`n", ",,,,,,,,,`n"); break }
+      "*Auth*" { $csvNode._label = "$_"; $csvNode._cols += ($Get_CSVSchema._auth); $csvNode._name = ("auth"); $csvNode._rows += @(",,,,,,,,,`n", ",,,,,,,,,`n", ",,,,,,,,,`n"); break }
+      "*Cart*" { $csvNode._label = "$_"; $csvNode._cols += ($Get_CSVSchema._cart); $csvNode._name = ("cart"); $csvNode._rows += @(",,,,,,,,,`n", ",,,,,,,,,`n", ",,,,,,,,,`n"); break }
+      "*Home*" { $csvNode._label = "$_"; $csvNode._cols += ($Get_CSVSchema._home); $csvNode._name = ("home"); $csvNode._rows += @(",,,,,,,,,`n", ",,,,,,,,,`n", ",,,,,,,,,`n"); break }
       { $_ -LIKE "*Product*" -OR $_ -LIKE "*Apparel*" -OR $_ -LIKE "*Art*" -OR $_ -LIKE "*Decor*" -OR $_ -LIKE "*Jewlery*" } {
-        $csvNode._label = "$_"; $csvNode._cols += ($CSVSchema._product); $csvNode._name = ("shop"); $csvNode._rows += @();
-        $csvNode._rel.toId = ( $csvNode._id ); $csvNode._rel.type = ("DESIGNS"); $csvNode._rel.direction = ('IN'); $csvNode._rel.properties = @{
-          id = [String]([System.Guid]::NewGuid().ToString()); date = [String](Get-Date); userId = [String](""); storeId = [String](""); productId = [String]("")
-        } ; break
+        $csvNode._label = "$_"; $csvNode._cols += ($Get_CSVSchema._product); $csvNode._name = ("shop"); $csvNode._rows += @(",,,,,,,,,`n", ",,,,,,,,,`n", ",,,,,,,,,`n");
+        $csvNode._rel.toId = ( $csvNode._id ); $csvNode._rel.type = ("DESIGNS"); $csvNode._rel.direction = ('IN'); $csvNode._rel.properties = ( $csvRelationship.properties ); break
       }
       { $_ -LIKE "*Profile*" -OR $_ -LIKE "*User*" -OR $_ -LIKE "*Teammate*" -OR $_ -LIKE "*Friend*" } {
-        $csvNode._label = "$_"; $csvNode._cols += ($CSVSchema._profile); $csvNode._name = ("profile"); $csvNode._rows += @();
-        $csvNode._rel.fromId = ( $csvNode._id ); $csvNode._rel.type = ("DESIGNS"); $csvNode._rel.direction = ('OUT'); $csvNode._rel.properties = @{
-          id = [String]([System.Guid]::NewGuid().ToString()); date = [String](Get-Date); userId = [String](""); storeId = [String](""); productId = [String]("")
-        } ; break
+        $csvNode._label = "$_"; $csvNode._cols += ($Get_CSVSchema._profile); $csvNode._name = ("profile"); $csvNode._rows += @(",,,,,,,,,`n", ",,,,,,,,,`n", ",,,,,,,,,`n");
+        $csvNode._rel.fromId = ( $csvNode._id ); $csvNode._rel.type = ("DESIGNS"); $csvNode._rel.direction = ('OUT'); $csvNode._rel.properties = ( $csvRelationship.properties ); break
       }
-      "*RBAC*" { $csvNode._label = "$_"; $csvNode._cols += ($CSVSchema._rbac); $csvNode._name = ("auth"); $csvNode._rows += @(); break }
-      "*Shop*" { $csvNode._label = "$_"; $csvNode._cols += ($CSVSchema._shop); $csvNode._name = ("shop"); $csvNode._rows += @(); break }
+      "*RBAC*" { $csvNode._label = "$_"; $csvNode._cols += ($Get_CSVSchema._rbac); $csvNode._name = ("auth"); $csvNode._rows += @(",,,,,,,,,`n", ",,,,,,,,,`n", ",,,,,,,,,`n"); break }
+      "*Shop*" { $csvNode._label = "$_"; $csvNode._cols += ($Get_CSVSchema._shop); $csvNode._name = ("shop"); $csvNode._rows += @(",,,,,,,,,`n", ",,,,,,,,,`n", ",,,,,,,,,`n"); break }
       default { break }
-    } ; $node_i += ( 1 ) ; $rel_i += ( 1 );
-    $SchemaNodes += @([Hashtable]( $csvNode ))
+    };
+    $Set_CSVSchema += @([Hashtable]( $csvNode )); $node_i += ( 1 ); $rel_i += ( 1 );
   }
 };
 <#  MAIN  |  NOE: for $id && $date attributes, use [System.Guid]::NewGuid() && Get-Date -F yyyy:MM:ddTHH:mm:ss.ms  #>
@@ -65,11 +65,9 @@ function New-CSVGraph([Parameter(Mandatory = $true)][Hashtable]$props) {
   } ; Start-Sleep( 9 );
   $props._nodes | ForEach-Object {
     [String]$data_path, $schema_path = "$($props._basePath)/data/$($_._name)", "$($props._basePath)/schema/$($_._name)";
-    try { ConvertFrom-Csv ("$( $_._cols )") -Delimiter (',') | Export-Csv ("$schema_path/$($_._label).csv") -NoTypeInformation } catch { throw };
-    try { ConvertFrom-Csv ("$( $_._rows )") -Delimiter (',') -Header ( $_._cols ) | Export-Csv ("$data_path/$($_._label).csv") -NoTypeInformation } catch { throw }
+    try { ConvertFrom-Csv ("$($_._cols)") -Delimiter (',') | Export-Csv ("$schema_path/$($_._label).csv") -NoTypeInformation } catch { throw };
+    try { ConvertFrom-Csv ("$($_._rows)") -Delimiter (',') -Header ($_._cols) | Export-Csv ("$data_path/$($_._label).csv") -NoTypeInformation } catch { throw }
   }
 };
 <#  DRIVER  |  Orchestrates calling Main() and FxHelpers() "safely"  #>
-if ( $configERROR ) { return } else {
-  try { New-CSVGraph @{ _basePath = [String]( $Neo4jPath ); _nodes = [System.Collections.ArrayList]( $SchemaNodes ) } } catch { throw ; return }
-}
+if ( $configERROR ) { return } else { try { New-CSVGraph @{ _basePath = [String]( $Neo4jPath ); _nodes = [System.Collections.ArrayList]( $Set_CSVSchema ) } } catch { throw ; return } }
